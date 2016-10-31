17eadc5b974bd4a5c1ce43aed7cb6c99785f763a
Andre Luiz Silva <andreluiz1013@hotmail.com>
2016-10-31 20:55:07 -0200

    Refatorando Model EnderecoEntrega

--- src/main/java/com/pedidovenda/model/EnderecoEntrega.java
+++ src/main/java/com/pedidovenda/model/EnderecoEntrega.java
@@ -4,30 +4,24 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 @Embeddable
 public class EnderecoEntrega implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;	
 	
-	@Column(name = "entrega_logradouro", nullable = false, length = 150)
 	private String logradouro;
-	
-	@Column(name = "entrega_numero", nullable = false, length = 20)
 	private String numero;
-	
-	@Column(name = "entrega_complemento", length = 150)
 	private String complemento;
-	
-	@Column(name = "entrega_cidade", nullable = false, length = 60)
 	private String cidade;
-	
-	@Column(name = "entrega_uf", nullable = false, length = 60)
 	private String uf;
-	
-	@Column(name = "entrega_cep", nullable = false, length = 9)
 	private String cep;
-
+	
+	@NotBlank @Size(max = 150)
+	@Column(name = "entrega_logradouro", nullable = false, length = 150)
 	public String getLogradouro() {
 		return logradouro;
 	}
@@ -35,7 +29,9 @@ public class EnderecoEntrega implements Serializable {
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
-
+	
+	@NotBlank @Size(max = 20)
+	@Column(name = "entrega_numero", nullable = false, length = 20)
 	public String getNumero() {
 		return numero;
 	}
@@ -43,7 +39,9 @@ public class EnderecoEntrega implements Serializable {
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-
+	
+	@Size(max = 150)
+	@Column(name = "entrega_complemento", length = 150)
 	public String getComplemento() {
 		return complemento;
 	}
@@ -51,7 +49,9 @@ public class EnderecoEntrega implements Serializable {
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
-
+	
+	@NotBlank @Size(max = 60)
+	@Column(name = "entrega_cidade", nullable = false, length = 60)
 	public String getCidade() {
 		return cidade;
 	}
@@ -59,7 +59,9 @@ public class EnderecoEntrega implements Serializable {
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-
+	
+	@NotBlank @Size(max = 60)
+	@Column(name = "entrega_uf", nullable = false, length = 60)
 	public String getUf() {
 		return uf;
 	}
@@ -67,7 +69,9 @@ public class EnderecoEntrega implements Serializable {
 	public void setUf(String uf) {
 		this.uf = uf;
 	}
-
+	
+	@NotBlank @Size(max = 9)
+	@Column(name = "entrega_cep", nullable = false, length = 9)
 	public String getCep() {
 		return cep;
 	}
