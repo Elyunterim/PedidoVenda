52e391e78771e7e14030c45ba157998664b03267
Andre Luiz Silva <andreluiz1013@hotmail.com>
2016-10-31 20:33:50 -0200

    Refatorando Model Categoria

--- src/main/java/com/pedidovenda/model/Categoria.java
+++ src/main/java/com/pedidovenda/model/Categoria.java
@@ -1,22 +1,30 @@
 package com.pedidovenda.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue
+		
 	private Long id;
 	private String descricao;
-	private Categoria origem;
+	private Categoria categoriaPai;
+	private List<Categoria> subcategorias = new ArrayList<>();
 
+	@Id
+	@GeneratedValue
 	public Long getId() {
 		return id;
 	}
@@ -24,7 +32,8 @@ public class Categoria implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
+	@Column(nullable = false, length = 60)
 	public String getDescricao() {
 		return descricao;
 	}
@@ -32,13 +41,24 @@ public class Categoria implements Serializable {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-
-	public Categoria getOrigem() {
-		return origem;
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_pai_id")
+	public Categoria getCategoriaPai() {
+		return categoriaPai;
 	}
 
-	public void setOrigem(Categoria origem) {
-		this.origem = origem;
+	public void setCategoriaPai(Categoria categoriaPai) {
+		this.categoriaPai = categoriaPai;
+	}
+	
+	@OneToMany(mappedBy = "categoriaPai", cascade = CascadeType.ALL)
+	public List<Categoria> getSubcategorias() {
+		return subcategorias;
+	}
+
+	public void setSubcategorias(List<Categoria> subcategorias) {
+		this.subcategorias = subcategorias;
 	}
 
 	@Override
